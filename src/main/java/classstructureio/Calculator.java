0d207045453e0bb042d59719dfc67370ecb39e7f
DZsJ <zsolt.doczi@gmail.com>
2020-11-14 11:50:58 +0100

    Java osztályok: Kiírás és beolvasás konzolról

---
+++ src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,21 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérek 2 számot:");
+        System.out.println("Az első? ");
+        int first = scanner.nextInt();
+        System.out.println();
+        System.out.println("A második? ");
+        int second = scanner.nextInt();
+        System.out.println();
+
+        System.out.println(first+ " + " + second + " = " + (first + second));
+    }
+}
