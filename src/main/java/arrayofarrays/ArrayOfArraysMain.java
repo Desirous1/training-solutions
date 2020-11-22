e59cd01ddc9e5e24bd4c05d790eb652735286975
DZsJ <zsolt.doczi@gmail.com>
2020-11-22 09:42:12 +0100

    Bonyolultabb típusok. Tömbök tömbje. Tömbök tömbjének kiíratása.

---
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -0,0 +1,39 @@
+package arrayofarrays;
+
+import array.ArrayHandler;
+
+public class ArrayOfArraysMain {
+
+    public int[][] multiplicationTable(int size) {
+
+        int[][] multiTabla = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int a = 0; a < size; a++) {
+                multiTabla[i][a] = (i+1)  * (a + 1);
+            }
+
+        }
+        return multiTabla;
+    }
+
+    public void printArrayOfArrays(int[][] a) {
+        for  (int i=0; i < a.length; i++) {
+            for (int q=0; q<a[i].length;q++) {
+                System.out.print(a[i][q] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ArrayOfArraysMain table = new ArrayOfArraysMain();
+        table.multiplicationTable(4);
+        table.printArrayOfArrays(table.multiplicationTable(4));
+
+    }
+
+
+
+}
