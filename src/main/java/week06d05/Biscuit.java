36cda6fc594ca1e6e917322eba4903c12593701e
DZsJ <zsolt.doczi@gmail.com>
2020-12-04 16:50:21 +0100

    week06d05 feladat

---
+++ src/main/java/week06d05/Biscuit.java
@@ -0,0 +1,41 @@
+package week06d05;
+
+public class Biscuit {
+
+    private BiscuitType type;
+    private int gramAmount;
+
+    public Biscuit(BiscuitType type, int gramAmount) {
+        this.type = type;
+        this.gramAmount = gramAmount;
+    }
+
+    public BiscuitType getType() {
+        return type;
+    }
+
+    public int getGramAmount() {
+        return gramAmount;
+    }
+
+    public void setGramAmount(int gramAmount) {
+        this.gramAmount = gramAmount;
+    }
+
+    public static Biscuit of(BiscuitType type, int gramAmount) {
+        return new Biscuit(type, gramAmount);
+    }
+
+
+        public static void main(String[] args) {
+            BiscuitType type = BiscuitType.PILOTA;
+
+            //type.setGramAmount(12);
+
+            System.out.println("Objektum címe:");
+            System.out.println(of(BiscuitType.PILOTA, 33));
+            System.out.println("Objektum állapota, változók értéke:");
+            System.out.println(type.toString() + " " + type.getGramAmount());
+        }
+
+}
---
+++ src/main/java/week06d05/BiscuitType.java
@@ -0,0 +1,19 @@
+package week06d05;
+
+public enum BiscuitType {
+        PILOTA(20), OREO(20), HIT(32);
+
+    private int gramAmount;
+
+        BiscuitType(int gramAmount) {
+                this.gramAmount = gramAmount;
+        }
+
+        public int getGramAmount() {
+                return gramAmount;
+        }
+
+        public void setGramAmount(int gramAmount) {
+            this.gramAmount = gramAmount;
+        }
+}
