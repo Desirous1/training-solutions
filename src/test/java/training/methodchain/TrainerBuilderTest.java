896d8edadeb4c7e995f8d509e3e6a0665a420f15
DZsJ <zsolt.doczi@gmail.com>
2020-12-07 19:31:40 +0100

    Metódus hívások láncolása

---
+++ src/test/java/training/methodchain/TrainerBuilderTest.java
@@ -0,0 +1,25 @@
+package training.methodchain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainerBuilderTest {
+    @Test
+
+    public  void   build() {
+        //TrainerBuilder trainerBuilder = new TrainerBuilder().withName("John Doe").withAge(30);
+        Trainer trainer = new TrainerBuilder()
+                .withName("John Doe")
+                .withAge(30)
+                .build();
+
+        //Trainer trainer = trainerBuilder.build();
+
+        assertEquals(trainer.getName(), "John Doe");
+    }
+
+
+
+
+}
