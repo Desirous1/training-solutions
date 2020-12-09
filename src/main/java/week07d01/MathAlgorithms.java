872fa6a60b7337666aa533de707c7714d1171c9d
DZsJ <zsolt.doczi@gmail.com>
2020-12-09 21:12:37 +0100

    week07d01 feladat

---
+++ src/main/java/week07d01/MathAlgorithms.java
@@ -0,0 +1,27 @@
+package week07d01;
+
+public class MathAlgorithms { // nem tudtam megoldani
+
+    public boolean isPrime(int x) {
+
+        if (x < 1) {
+            throw new IllegalArgumentException("Wrong number!");
+        }
+
+        if (x == 1) {
+        return true;
+        }
+
+        if (x == 2) {
+        return true;
+        }
+
+        for(int i = 2; i < Math.sqrt(x); i++) {
+            if (x % i == 0) {
+            return false;
+            }
+        }
+    return true;
+    }
+}
+
