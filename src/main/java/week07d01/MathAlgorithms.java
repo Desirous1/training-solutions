e4a5d7d604b23956d763bae7090f2eade9b174c5
DZsJ <zsolt.doczi@gmail.com>
2020-12-09 21:30:41 +0100

    week07d01 feladat

--- src/main/java/week07d01/MathAlgorithms.java
+++ src/main/java/week07d01/MathAlgorithms.java
@@ -2,21 +2,21 @@ package week07d01;
 
 public class MathAlgorithms { // nem tudtam megoldani
 
-    public boolean isPrime(int x) {
+    public static boolean isPrime(int x) {
 
         if (x < 1) {
             throw new IllegalArgumentException("Wrong number!");
         }
 
         if (x == 1) {
-        return true;
+        return false;
         }
 
         if (x == 2) {
         return true;
         }
 
-        for(int i = 2; i < Math.sqrt(x); i++) {
+        for(int i = 2; i <= Math.sqrt(x); i++) {
             if (x % i == 0) {
             return false;
             }
