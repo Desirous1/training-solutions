b7d6365b495a177ab79e4b0e65ae3b7f450cf5d8
DZsJ <zsolt.doczi@gmail.com>
2020-12-14 13:00:33 +0100

    week04d01

---
+++ src/test/java/week04d01/NameChangerTest.java
@@ -0,0 +1,16 @@
+package week04d01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameChangerTest {
+
+    @Test
+    public void testNameChanger() {
+        NameChanger nameChanger = new NameChanger("Dóczi Zsolt");
+        System.out.println(nameChanger);
+        assertEquals("Csányi Zsolt", nameChanger.changeFirstName("Csányi"));
+        System.out.println(nameChanger.changeFirstName("Csányi"));
+    }
+}
