4aa26466669e1ee96f40c1c7af1592416a273b83
DZsJ <zsolt.doczi@gmail.com>
2020-12-14 18:02:41 +0100

    week07d04

---
+++ src/test/java/wek07d04/LabTest.java
@@ -0,0 +1,43 @@
+package wek07d04;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabTest {
+
+    @Test
+    public void testTitle() {
+        Lab lab = new Lab("Nyelvi elemek");
+        assertEquals(false, lab.isCompleted());
+    }
+
+    @Test
+    public void testDate() {
+        Lab lab = new Lab("Nyelvi", LocalDate.of(2020, 12, 21));
+        System.out.println(lab.isCompleted());
+        assertEquals(true, lab.isCompleted());
+    }
+
+    @Test
+    public void testComplete() {
+        Lab lab = new Lab("a");
+        System.out.println(lab.isCompleted() + " " + lab.getCompletedAt());
+        lab.complete(LocalDate.of(2020,12,12));
+        System.out.println(lab.isCompleted() + " " + lab.getCompletedAt());
+        assertEquals(true, lab.isCompleted());
+    }
+
+    @Test
+    public void testCompleteNull() {
+        Lab lab = new Lab("a");
+        System.out.println(lab.isCompleted() + " " + lab.getCompletedAt());
+        lab.complete();
+        System.out.println(lab.isCompleted() + " " + lab.getCompletedAt());
+        assertEquals(true, lab.isCompleted());
+    }
+
+}
