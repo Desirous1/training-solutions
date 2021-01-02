09488e8bb2fd0fed5663ec133d2c3fcbc01f6201
DZsJ <zsolt.doczi@gmail.com>
2021-01-02 19:30:34 +0100

    Dinamikus kötés..

--- src/test/java/virtualmethod/trainer/TrainerTest.java
+++ src/test/java/virtualmethod/trainer/TrainerTest.java
@@ -20,7 +20,6 @@ class TrainerTest {
         assertEquals(3, trainer.getFreeTime());
 
         Human human = new Trainer("John Doe", Arrays.asList(new Course("Course1")));
-        assertEquals(3,human.getFreeTime());
-
+        assertEquals(3, human.getFreeTime());
     }
 }
