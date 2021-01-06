f6bf48733e84cf2de4cdd12ec0c3a56a337ab5bf
DZsJ <zsolt.doczi@gmail.com>
2021-01-06 12:42:30 +0100

    öröklődés. konstruktor működése

---
+++ src/main/java/training/proba/Animal.java
@@ -0,0 +1,8 @@
+package training.proba;
+
+public class Animal {
+
+    public Animal() {
+        System.out.println("animal");
+    }
+}
---
+++ src/main/java/training/proba/Bear.java
@@ -0,0 +1,8 @@
+package training.proba;
+
+public class Bear extends Animal{
+
+    public static void main(String[] args) {
+        new Bear();
+    }
+}
