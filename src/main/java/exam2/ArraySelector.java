21dd21de6cb3c4623ff057654da7811a60300e28
DZsJ <zsolt.doczi@gmail.com>
2021-01-06 19:37:09 +0100

    exam2 Tömb end test ok

--- src/main/java/exam2/ArraySelector.java
+++ src/main/java/exam2/ArraySelector.java
@@ -1,4 +1,27 @@
 package exam2;
+/*
+Írj egy olyan metódust az ArraySelector osztályban, mely egy kapott tömbből minden második elemet kiválogat,
+és egy szövegként összerak, szögletes zárójelek között!
+A páros megállapítása index szerint működik. Azaz az első, nulla indexű elem páros,
+ a második, azaz egyes indexű elem páratlan, stb.
+
+Ha nulla elemű a tömb, akkor üres stringet adjon vissza!
+ */
+
 
 public class ArraySelector {
+
+    private StringBuilder result = new StringBuilder("[");
+
+    public String selectEvens(int[] paraTomb){
+        if (paraTomb.length==0) {
+            return "";
+        }
+
+        for (int i=0; i < paraTomb.length;i=i+2) {
+            result.append(paraTomb[i]).append(", ");
+        }
+        result.deleteCharAt(result.length()-1).deleteCharAt(result.length()-1).append("]");
+        return result.toString();
+    }
 }
