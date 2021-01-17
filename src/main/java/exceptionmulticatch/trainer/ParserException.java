43609ff91a7fea89f639ee9e74b4f69d88a9dca7
DZsJ <zsolt.doczi@gmail.com>
2021-01-17 12:50:16 +0100

    multi catch

---
+++ src/main/java/exceptionmulticatch/trainer/ParserException.java
@@ -0,0 +1,7 @@
+package exceptionmulticatch.trainer;
+
+public class ParserException extends RuntimeException {
+    public ParserException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
---
+++ src/main/java/exceptionmulticatch/trainer/TrainerParser.java
@@ -0,0 +1,18 @@
+package exceptionmulticatch.trainer;
+
+import javax.swing.text.html.parser.Parser;
+import java.text.ParseException;
+
+public class TrainerParser {
+    public static final String SEPARATOR = ";";
+
+    public Trainer parse(String line) {
+        try {
+            String[] fields = line.split(SEPARATOR);
+            return new Trainer(fields[0], Integer.parseInt(fields[1]));
+        }
+        catch (NullPointerException | IllegalArgumentException | IndexOutOfBoundsException e) {
+            throw new ParserException("Invalid line: " + line, e);
+        }
+    }
+}
