3172410fafcb488e40fa9df2996f3787f95a9aab
DZsJ <zsolt.doczi@gmail.com>
2021-01-17 11:34:23 +0100

    activity, catalog, week11d04

---
+++ src/main/java/week11d04/FilesSum.java
@@ -0,0 +1,28 @@
+package week11d04;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilesSum {
+
+    private String prefixName = "number";
+    private List<String> fileNameList = new ArrayList<>();
+
+
+    public void fileListGenerate() {
+
+    }
+
+
+
+
+    public int sumNumbers(InputStream paraName) {
+     //   int summaValue = Integer.parseInt(Files.readString("number00.txt"));
+return 0;
+    }
+
+
+
+}
