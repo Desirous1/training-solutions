fa7c3070dd73225ac7a8ae1004bcb19b90a0fc0a
DZsJ <zsolt.doczi@gmail.com>
2021-02-02 21:28:11 +0100

    week12d04 fejl..

--- src/main/java/week12d04/Decoder.java
+++ src/main/java/week12d04/Decoder.java
@@ -1,19 +1,28 @@
 package week12d04;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Decoder {
     public void decode(Path path) {
-        try {
-            byte[] bytes = Files.readAllBytes(path);
-            for (byte b : bytes) {
-                char c = (char) (b + 10);
-                System.out.print(c);
+        //1try {
+        try (InputStream is = Files.newInputStream(path)) {
+            //1 byte[] bytes = Files.readAllBytes(path);
+            byte[] buffer = new byte[10];
+            //1for (byte b : bytes) {
+            int size;
+            while( (size = is.read(buffer)) > 0 ){
+                //for (byte b : bytes) {
+                for (int i=0; i< size;i++) {
+                    char c = (char) (buffer[i] + 10);
+                    System.out.print(c);
+                }
+                //1 }
             }
         }
-        catch (IOException ioe){
+         catch (IOException ioe) {
             throw new IllegalStateException("", ioe);
         }
     }
