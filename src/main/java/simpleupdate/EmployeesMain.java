a2f7ca7e0fd96a0d1a292376896080fe966dc2dd
DZsJ <zsolt.doczi@gmail.com>
2021-03-01 16:45:01 +0100

    Simple update JDBC leckék .

--- src/main/java/simpleupdate/EmployeesMain.java
+++ src/main/java/simpleupdate/EmployeesMain.java
@@ -17,30 +17,30 @@ public class EmployeesMain {
         }
 
         // adatbázisba insertálás/írás
-//            try (
-//                    Connection conn = dataSource.getConnection();
-//                    PreparedStatement stmt =
-//                            conn.prepareStatement("INSERT INTO employees(emp_name) VALUES (?)")) {
-//                stmt.setString(1,"John Doe");
-//                stmt.executeUpdate();
-//            } catch (SQLException se) {
-//                throw new IllegalStateException("Can not insert", se);
-//            }
-
-        // adatbázis lekérdezés
         try (
                 Connection conn = dataSource.getConnection();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("select emp_name, id from employees");
+                PreparedStatement stmt = conn.prepareStatement("INSERT INTO employees(emp_name) VALUES (?)")
         ) {
-            while (rs.next()) {
-                //String name = rs.getString("emp_name");
-                //int idn = rs.getInt("id");
-                //System.out.println(name+ " " + idn);
-                System.out.println(rs.getInt("id") + " " + rs.getString("emp_name"));
-            }
+            stmt.setString(1, "John Doe");
+            stmt.executeUpdate();
         } catch (SQLException se) {
-            throw new IllegalStateException("Cannot select employees", se);
+            throw new IllegalStateException("Can not insert", se);
         }
+
+        // adatbázis lekérdezés
+//        try (
+//                Connection conn = dataSource.getConnection();
+//                Statement stmt = conn.createStatement();
+//                ResultSet rs = stmt.executeQuery("select emp_name, id from employees");
+//        ) {
+//            while (rs.next()) {
+//                //String name = rs.getString("emp_name");
+//                //int idn = rs.getInt("id");
+//                //System.out.println(name+ " " + idn);
+//                System.out.println(rs.getInt("id") + " " + rs.getString("emp_name"));
+//            }
+//        } catch (SQLException se) {
+//            throw new IllegalStateException("Cannot select employees", se);
+//        }
     }
 }
